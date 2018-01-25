48929ad4ee07624423547b4f8b53675a2acb2447
Powerless <wzxaini9@163.com>
2018-01-25 09:49:40 +0800

    前端页面鼠标特效

---
+++ mouse_attract/README.md
@@ -0,0 +1,7 @@
+# ThinkCMF-mouse_attract
+鼠标吸附效果
+ThinkCMF-前台页面鼠标特效插件
+1.0.0版 
+https://www.thinkcmf.com/appstore/plugin/141.html
+解压至项目public\plugins\目录下即可。
+已在5.0.180123 版测试通过
---
+++ mouse_attract/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'mouse' => [// 在后台插件配置表单中的键名 ,会是config[text]
+        'title' => '点的数量:', // 表单的label标题
+        'type' => 'number',// 表单的类型：text,password,textarea,checkbox,radio,select等
+        'value' => '100',// 表单的默认值
+    ],
+    'color' => [// 在后台插件配置表单中的键名 ,会是config[text]
+        'title' => '线的颜色:', // 表单的label标题
+        'type' => 'text',// 表单的类型：text,password,textarea,checkbox,radio,select等
+        'value' => '32,128,255',// 表单的默认值
+    ],
+];
+					
