059a68f8bf0bd1043bbbbfb348ac372bafb7237f
Powerless <wzxaini9@163.com>
2018-03-08 12:13:54 +0800

    由于5.0.180123之前版本没有PluginAdminBaseController,所以改为PluginBaseController;方便兼容旧版本

--- admin_journal/controller/AdminIndexController.php
+++ admin_journal/controller/AdminIndexController.php
@@ -2,25 +2,34 @@
 // +----------------------------------------------------------------------
 // | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2013-2014 http://www.thinkcmf.com All rights reserved.
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: Dean <zxxjjforever@163.com>
+// | Author: Powerless <wzxaini9@gmail.com>
 // +----------------------------------------------------------------------
-namespace plugins\admin_journal\controller; //Demo插件英文名，改成你的插件英文就行了
 
-use cmf\controller\PluginAdminBaseController;
+namespace plugins\admin_journal\controller;
+
+use cmf\controller\PluginBaseController;
 
 
-class AdminIndexController extends PluginAdminBaseController
+class AdminIndexController extends PluginBaseController
 {
 
     protected function _initialize()
     {
-        $adminId = cmf_get_current_admin_id();//获取后台管理员id，可判断是否登录
+        $adminId = cmf_get_current_admin_id();
         if (!empty($adminId)) {
+            if (!$this->checkAccess($adminId)) {
+                $this->error("您没有访问权限！");
+            }
             $this->assign("admin_id", $adminId);
         } else {
-            $this->error('未登录');
+            if ($this->request->isAjax()) {
+                $this->error("您还没有登录！", url("admin/Public/login"));
+            } else {
+                header("Location:" . url("admin/Public/login"));
+                exit();
+            }
         }
     }
 
@@ -61,5 +70,25 @@ class AdminIndexController extends PluginAdminBaseController
         $this->assign('time', $date);
         $this->assign("num",$num);
         return $this->fetch('/admin_index');
+    }
+
+    /**
+     *  检查后台用户访问权限
+     * @param int $userId 后台用户id
+     * @return boolean 检查通过返回true
+     */
+    private function checkAccess($userId)
+    {
+        // 如果用户id是1，则无需判断
+        if ($userId == 1) {
+            return true;
+        }
+
+        $pluginName = $this->request->param('_plugin');
+        $controller = $this->request->param('_controller');
+        $controller = cmf_parse_name($controller, 1);
+        $action     = $this->request->param('_action');
+
+        return cmf_auth_check($userId, "plugin/{$pluginName}/$controller/$action");
     }
 }
