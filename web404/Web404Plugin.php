a0065be4696cad4f3e15caecae1d8d8836e32cb9
Powerless <wzxaini9@163.com>
2021-04-15 17:13:00 +0800

    404适配6.0

--- web404/Web404Plugin.php
+++ web404/Web404Plugin.php
@@ -21,21 +21,26 @@ class Web404Plugin extends Plugin
         'version'     => '2.0.0'
     ];
 
-    public $hasAdmin = 0;//插件是否有后台管理界面
-
     // 插件安装
     public function install()
     {
+        cmf_set_dynamic_config([
+            'app' => [
+                'exception_tmpl' => CMF_ROOT . 'public/plugins/web404/404.html' // 404页面可自定义修改
+            ]
+        ]);
         return true;//安装成功返回true，失败false
     }
 
     // 插件卸载
     public function uninstall()
     {
+        // 动态修改异常页面路径
+        cmf_set_dynamic_config([
+            'app' => [
+                'exception_tmpl' => CMF_ROOT . 'vendor/thinkphp/tpl/think_exception.tpl'
+            ]
+        ]);
 		return true;//卸载成功返回true，失败false
     }
-	public function appBegin()
-	{
-		config('exception_tmpl',PLUGINS_PATH.'web404/404.html');
-	}
 }
