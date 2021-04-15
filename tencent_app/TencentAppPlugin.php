a6b4ba339dde9ab3b0e7be7358dd5203287205a7
Powerless <wzxaini9@163.com>
2021-04-15 15:32:46 +0800

    1.0

--- tencent_app/TencentAppPlugin.php
+++ tencent_app/TencentAppPlugin.php
@@ -19,7 +19,7 @@ class TencentAppPlugin extends Plugin
         'description' => '腾讯小程序管理工具',
         'status'      => 1,
         'author'      => 'Powerless',
-        'version'     => '1.0',
+        'version'     => '1.0.0',
         'demo_url'    => 'https://wzxaini9.cn',
         'author_url'  => 'https://wzxaini9.cn'
     ];
